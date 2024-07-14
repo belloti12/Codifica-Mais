6986b37615b7262282516d48baad0c2fa89e4e2e
João Pedro Belloti Góes <jpbelloti12@gmail.com>
2024-07-14 20:24:36 -0300

    exercicio 11 - Feito

--- PHP-ARRAYS/php-b11.php
+++ PHP-ARRAYS/php-b11.php
@@ -73,54 +73,55 @@ $estoque = [
     ]
 ];
 
-// Função que verifica o codigo SKU escolhido pelo usuario
-function verifica_key(array $estoque, &$sku) 
+// Função que verifica se o codigo SKU escrito pelo usuario ja existe, ele só sai da função com um codigo SKU possivel
+function verifica_key(array $estoque, string &$sku)
 {
-
+    $existe = false;
     foreach ($estoque as $array) {
-        // Se o codigo do usuario for igual ao codigo SKU do produto, a função pede outro codigo SKU
         if ($sku == $array["sku"]) {
-            $sku = readline("O codigo SKU $sku ja existe !!!, insira outro: ");
-            // Caso o usuario escreva outro codigo ja existente, a função é chamada novamente
-            verifica_key($estoque, $sku);
+            $existe = true;
         }
     }
+    return $existe;
+
 }
 
 // Função que adiciona produtos (1)
-function adicionarProduto(&$estoque, $sku, $nome, $unidade_medida, $quantidade, $preco) 
+function adicionarProduto(&$estoque, string $sku, string $nome, string $unidade_medida, int $quantidade, float $preco) : void 
 {
-
-    // Função que verifica se o codigo SKU escrito pelo usuario ja existe, ele só sai da função com um codigo SKU possivel
-    verifica_key($estoque, $sku);
 
     // Adiciona o produto no estoque
     $estoque[] = [
-        // O ucwords só padroniza para Title Case
-        "sku" => ucwords($sku),
-        "nome" => ucwords($nome),
+        "sku" => $sku,
+        "nome" => $nome,
         "unidade_medida" => $unidade_medida,
         "quantidade" => $quantidade,
         "preco" => $preco
     ]; 
+
+    echo "O produto $nome de codigo SKU $sku foi adicionado." . PHP_EOL;
+
 }
 
 // Função que vende algum item (2)
-function vende_item(array &$estoque, $codigo, $sku)
+function vende_item(array &$estoque, string $sku) : void
 {
+
+    $codigo = array_search($sku, array_column($estoque, "sku"));
+
     while (true) {
 
-        $quantidade = readline("Quantidade de {$estoque[$codigo]["nome"]} que voce vendeu: ");
+        $quantidade = trim(readline("Quantidade de {$estoque[$codigo]["nome"]} que voce vendeu: "));
 
         $produto = $estoque[$codigo]["nome"];
         $quantidade_velha = $estoque[$codigo]["quantidade"];
 
         // Para o caso do estoque ser esgotado
         if ($quantidade_velha - $quantidade == 0) {
-            unset($estoque[$codigo]);
+            $estoque[$codigo]["quantidade"] = 0;
             echo "..." . PHP_EOL;            
             echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" . PHP_EOL;
-            echo "O produto " . $produto . ", de codigo SKU " . $sku . " foi esgotado, portanto foi removido do estoque" . PHP_EOL;
+            echo "O produto " . $produto . ", de codigo SKU " . $sku . " foi esgotado, portanto foi zerado" . PHP_EOL;
             echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" . PHP_EOL;
             break;
         }
@@ -144,7 +145,7 @@ function vende_item(array &$estoque, $codigo, $sku)
     }
 }
 
-function lista_item_especifico ($array) 
+function lista_item_especifico (array $array) : void 
 {
     echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" . PHP_EOL;         
 
@@ -158,14 +159,14 @@ function lista_item_especifico ($array)
 }
 
 // Função que verifica o estoque (3)
-function verifica_estoque (array $estoque, $escolha) 
+function verifica_estoque (array $estoque, string $escolha) : void 
 {
     // Variavel que guarda se o item existe ou não no estoque
     $existe = false;
 
     // Condição dependendo da escolha do usuario
     if ($escolha == "N" || $escolha == "n") {
-        $nome = ucwords(readline("Otimo!, qual é o nome do produto?: "));
+        $nome = trim(ucwords(readline("Otimo!, qual é o nome do produto?: ")));
 
         // Loop que percorre o estoque inteiro, comparando o nome informado pelo usuario com o nome dos produtos
         foreach ($estoque as $array) {
@@ -203,31 +204,26 @@ function verifica_estoque (array $estoque, $escolha)
     }
 }
 
+// Função auxiliar para o usort()
+function ordena_alfabeticamente(array $nome1, array $nome2) : int 
+{
+    return $nome1["nome"] <=> $nome2["nome"];
+}
+
 // Função que lista os itens (4)
 function lista_itens (array &$estoque) 
 {
 
-    // Cria um array auxiliar, a key de cada elemento é seu nome e o valor é o array que o nome esta presente
-    $array_auxiliar = [];
-
-    foreach ($estoque as $array) {
-        $array_auxiliar[$array["nome"]] = $array;
-    }
-
-    // Organiza a lista em ordem alfabetica, considerando a key dos elementos
-    ksort($array_auxiliar);
-
-    echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" . PHP_EOL;
-    
-    foreach ($array_auxiliar as $array) {
-        // Percorre o array de cada produto, imprimindo cada categoria e valor da categoria
-        foreach ($array as $key => $item) {
-            echo $key . ": " . $item . " | ";
-        }         
+    // Ordena em ordem alfabetica e lista os produtos, mantendo o indice original
+    uasort($estoque, "ordena_alfabeticamente");
+    foreach ($estoque as $indice => $array_produto) {
+        echo "$indice => ";
+        foreach ($array_produto as $categoria => $info) {
+            echo "$categoria: $info | ";
+        }
         echo PHP_EOL;
-
     }
-    echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" . PHP_EOL;            
+
 }
 
 // Função para exibir o menu e obter a escolha do usuário
@@ -240,11 +236,77 @@ function exibirMenu()
     echo "2. Vender um produto \n";
     echo "3. Verificar Estoque \n";
     echo "4. Listar o Estoque \n";
-    echo "5. Sair \n";
+    echo "5. Atualizar o Estoque \n"; 
+    echo "6. Quantidade de Itens no Estoque \n"; 
+    echo "7. Deletar um produto do Estoque \n"; 
+    echo "8. Sair \n";
     echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" . PHP_EOL;
-    $opcao = readline("Digite a sua escolha: ");
+    $opcao = trim(readline("Digite a sua escolha: "));
     echo PHP_EOL;
     return $opcao;
+}
+
+// Função que exige que o usuario digite o tipo desejado para a variavel
+function tipo_certo(&$categoria, string $nome_categoria, $tipo) : void
+{
+    if ($tipo == "string") {
+        while (is_numeric($categoria)) {
+            $categoria = trim(readline("$nome_categoria do produto deve estar em string ! Tente novamente: "));
+        }
+    }
+    else {
+        while (is_numeric($categoria) == false) {
+            $categoria = trim(readline("$nome_categoria do produto deve estar em $tipo ! Tente novamente: "));
+        }
+        if ($tipo == "int") {
+            $categoria = (int) $categoria;
+        }
+    }
+}
+
+// Função que atualiza a quantidade de um produto
+function atualizar_produto(array &$estoque, string $sku, $quantidade) : void
+{
+    $codigo = array_search($sku, array_column($estoque, "sku"));
+
+    $estoque[$codigo]["quantidade"] += $quantidade;
+}
+
+// Função que imprimi a quantidade de produtos e a soma dessas quantidade
+function quantidade_produtos(array $estoque) : void
+{
+    $soma_quantidade = 0;
+
+    foreach ($estoque as $produto) {
+        $soma_quantidade += $produto["quantidade"];
+    }
+
+    $quantidade_de_produtos = count($estoque);
+
+    echo "Há $quantidade_de_produtos produtos no estoque" . PHP_EOL;
+    echo "A soma das quantidades de produtos totais é $soma_quantidade" . PHP_EOL;
+
+}
+
+// Função que deleta um item do estoque
+function deletar_produto(array &$estoque, string $sku) : void 
+{
+    $codigo = array_search($sku, array_column($estoque, "sku"));
+
+    $quantidade = $estoque[$codigo]["quantidade"];
+    $nome = $estoque[$codigo]['nome'];
+
+    if ($quantidade != 0) {
+        $resposta = trim(readline("O produto $nome ainda tem o estoque de $quantidade, deseja mesmo deleta-lo? [S/N]: "));
+    }
+    else {
+        $resposta = "S";
+    }
+
+    if (strtoupper($resposta)[0] == "S") {
+        echo "O produto $nome foi deletado do estoque." . PHP_EOL;
+        unset($estoque[$codigo]);
+    }
 }
 
 while (true) {
@@ -253,39 +315,83 @@ while (true) {
 
     switch ($opcao) {
         case 1:
-            $sku = readline("Digite o Código SKU do produto: ");
-            verifica_key($estoque, $sku);
-            $nome = readline("Digite o Nome do produto: ");
-            $unidade_medida = readline("Digite a Unidade de Medida: ");
-            $quantidade = readline("Digite a Quantidade: ");
-            $preco = readline("Digite o preço do produto: ");
+            $sku = trim(readline("Digite o Código SKU do produto: "));
+            tipo_certo($sku, "O codigo SKU", "string");
+            
+            while (verifica_key($estoque, $sku) == true) {
+                $sku = trim(readline("O codigo SKU $sku ja existe !!!, insira outro: "));                
+                // O usuario tem que escreva um codigo SKU que não exista
+            }
+        
+            $nome = trim(ucwords(readline("Digite o Nome do produto: ")));
+            tipo_certo($nome, "O nome", "string");
+
+            $unidade_medida = trim(ucwords(readline("Digite a Unidade de Medida: ")));
+            tipo_certo($unidade_medida, "A unidade de medida", "string");
+
+            $quantidade = trim(readline("Digite a Quantidade: "));
+            tipo_certo($quantidade, "A quantidade", "int");
+
+            $preco = trim(readline("Digite o preço do produto: "));
+            tipo_certo($preco, "O preço", "float");
+
             adicionarProduto($estoque, $sku, $nome, $unidade_medida, $quantidade, $preco);
             break;
         case 2:
-            $sku = readline("Qual é o codigo SKU do produto que voce vendeu?: ");
+            $sku = trim(readline("Qual é o codigo SKU do produto que voce vendeu?: "));
+            tipo_certo($sku, "O codigo SKU", "string");
 
-            $codigo = array_search($sku, array_column($estoque, "sku"));
+            while (verifica_key($estoque, $sku) == false) {
+                $sku = trim(readline("O codigo SKU $sku NÃO existe !!!, insira outro: "));                
+                // O usuario tem que escrever um SKU valido para continuar
+            }
 
-            vende_item($estoque, $codigo, $sku);
-
+            vende_item($estoque, $sku);
             break;
         case 3:
-
-            $escolha = readline("Gostaria de informar o nome ou codigo SKU do produto? [N/C]: ");
+            $escolha = strtoupper(trim(readline("Gostaria de informar o nome ou codigo SKU do produto? [N/C]: ")))[0];
+            tipo_certo($escolha, "Sua escolha ", "string");
 
             while ($escolha != "n" && $escolha != "N" && $escolha != "c" && $escolha != "C") {
-                $escolha = readline("Gostaria de informar o nome ou codigo SKU do produto? [N/C]: ");
+                $escolha = strtoupper(trim(readline("Gostaria de informar o nome ou codigo SKU do produto? [N/C]: ")))[0];
+                tipo_certo($escolha, "Sua escolha ", "string");
             }
 
             verifica_estoque($estoque, $escolha);
-
             break;
         case 4:
-
             lista_itens($estoque);
-
             break;
         case 5:
+            $sku = trim(readline("Qual é o codigo SKU do produto que voce deseja atualizar?: "));
+            tipo_certo($sku, "O codigo SKU", "string");
+
+            while (verifica_key($estoque, $sku) == false) {
+                $sku = trim(readline("O codigo SKU $sku NÃO existe !!!, insira outro: "));   
+                tipo_certo($sku, "O codigo SKU", "string");
+                // O usuario tem que escrever um SKU valido para continuar
+            }
+
+            $quantidade = trim(readline("Qual é a quantidade para acrescentar?: "));
+            tipo_certo($quantidade, "A quantidade", "int");
+
+            atualizar_produto($estoque, $sku, $quantidade);
+            break;
+        case 6:
+            quantidade_produtos($estoque);
+            break;
+        case 7:
+            $sku = trim(readline("Qual é o codigo SKU do produto que voce deseja deletar?: "));
+            tipo_certo($sku, "O codigo SKU", "string");
+
+            while (verifica_key($estoque, $sku) == false) {
+                $sku = trim(readline("O codigo SKU $sku NÃO existe !!!, insira outro: "));                
+                // O usuario tem que escrever um SKU valido para continuar
+            }
+
+            deletar_produto($estoque, $sku);
+            break;
+        case 8:
             echo "Saindo...\n";
             exit; // Sai do loop e encerra o script
         default:
