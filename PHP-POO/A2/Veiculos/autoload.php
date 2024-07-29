0846e053a2c7379a4470e07cf3d967385f5721df
João Pedro Belloti Góes <jpbelloti12@gmail.com>
2024-07-29 17:54:46 -0300

    Organizar pastas

--- PHP-POO/A2/Veiculos/autoload.php
+++ PHP-POO/A2/Veiculos/autoload.php
@@ -3,12 +3,14 @@
 
 spl_autoload_register(function (string $nomeCompletoDaClasse) {
 
-    //echo $nomeCompletoDaClasse;
-    //exit();
+    // echo $nomeCompletoDaClasse;
+    // exit();
     // $caminhoArquivo = str_replace('Seu\\NameSpace', 'src', $nomeCompletoDaClasse);
     $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $nomeCompletoDaClasse);
+    $caminhoArquivo = "PHP-POO\\A2\\" . $caminhoArquivo;
     $caminhoArquivo .= ".php";
-
+    // echo $caminhoArquivo;
+    // exit();
     if (file_exists($caminhoArquivo)) {
         require_once $caminhoArquivo;
     }
